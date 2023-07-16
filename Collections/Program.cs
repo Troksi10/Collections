eaf29194ae6dac03b4b74d0e192a92f5ca25c507
lLija <ilija.trokovski@gmail.com>
2023-07-17 00:47:30 +0200

    foreach && Arrays

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,5 +1,4 @@
-﻿
-// Arrays
+﻿// Arrays
 int[] grades = new int[5];
 
 grades[0] = 20;
@@ -26,4 +25,79 @@ int[] gradesOfStudents = new int[] {15,20,8,12,5,6,17};
 
 Console.WriteLine($"Length of math students is : {gradesOfMathStudents.Length}");
 
+// Foreach loop
+
+int[] nums = new int[10];
+int counter = 0;
+
+foreach(int i in nums)
+{
+    Console.WriteLine("Element{0} = {1}",counter,i);
+}
+
+// Create an array with 5 of your best friends
+// Create a foreach loop which greets all of them
+
+string[] myFriends = { "Bojan", "Stefan", "Kosta", "Martin", "Sasho" };
+
+
+foreach (string name in myFriends)
+{
+
+    Console.WriteLine($"Hi there {name},my friend.");
+}
+
+int[] numbers = {1,2,3,4,5};
+
+foreach (int number in numbers)
+{
+    Console.WriteLine($"Here are the number : {number}");
+}
+
+// Multi-Dimensional Array
+
+// declare 2D Array
+string[,] matrixOne;
+
+// 3D Array
+int[,,] threeD;
+
+// two dimensional array
+
+int[,] array2D = new int[,]
+{
+    {1,2,3},// row 0
+    {4,5,6},// row 1
+    {7,8,9} // row 2
+};
+
+Console.WriteLine($"Central value is {array2D[1,1]}");
+Console.WriteLine(array2D[2,0]); // 7
+
+int[,] matrix =
+{
+    {1,2,3},
+    {4,5,6},
+    {7,8,9}
+};
+
+foreach (int item in matrix)
+{
+    Console.Write(item + "");
+}
+
+Console.WriteLine("\n This is our 2D array printed using nested for loop");
+
+// Nested for loop
+// outer for loop
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    //inner for loop
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        Console.WriteLine(matrix[i,j] + "");
+    }
+}
+
 Console.ReadLine();
+
